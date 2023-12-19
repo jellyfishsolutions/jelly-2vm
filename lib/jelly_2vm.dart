a864d215f263823b59c3848172b8c8a95caff27e
alberto.conte <alberto.conte@appfactory.it>
2023-12-19 11:40:17 +0100

    Enforced lowercase_with_underscores file naming

--- lib/jelly_2vm.dart
+++ lib/jelly_2vm.dart
@@ -1,6 +1,6 @@
 library jelly_2vm;
 
-export 'src/view-model.dart' show ViewModel;
-export 'src/view-widget.dart' show ViewWidget;
-export 'src/utils/change-builder.dart' show ChangeBuilder;
-export 'src/utils/view-model-delegator.dart' show ViewModelDelegator;
+export 'src/view_model.dart' show ViewModel;
+export 'src/view_widget.dart' show ViewWidget;
+export 'src/utils/change_builder.dart' show ChangeBuilder;
+export 'src/utils/view_model_delegator.dart' show ViewModelDelegator;
