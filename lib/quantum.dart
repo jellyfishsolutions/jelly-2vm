6f6f6a5ba2f61f15216e21d3e6f0db682a0ca6cb
alberto.conte <alberto.conte@appfactory.it>
2023-12-22 15:00:08 +0100

    Fixed export

--- lib/quantum.dart
+++ lib/quantum.dart
@@ -5,3 +5,5 @@ export 'src/quantum/entities/molecules/molecules.dart';
 export 'src/quantum/entities/aggregator.dart';
 export 'src/quantum/builders/quantum_builders.dart' show QuantumBuilder;
 export 'src/quantum/builders/aggregator_builder.dart' show AggregatorBuilder;
+export 'src/quantum/interfaces/persistent_storage_interface.dart'
+    show AtomPersistentStorageInterface;
